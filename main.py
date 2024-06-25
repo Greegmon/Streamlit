754fca216bb02b5acf7922b36bac490829836de6
Greegmon <149600509+Greegmon@users.noreply.github.com>
2024-06-25 09:56:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,4 @@ Facebook : https://www.facebook.com/61555393773104
 - Kunin mo na ang source code wag lang ang ka pogian ko 😎
 - Sa china nalang ang WPS (West Philippine Sea)
 basta satin ang SCS (South China Sea)
-
--------- NOTE ! --------
-kung gagamitin mo tong src nato okay lang
-pero punta ka muna sa ./streamlit/secrets.toml
-palitan mo yung DB_URL & DB_KEY ng database mo.
-
-TABLE NAME : accounts
-_____________________________________________
-| id | username | password | role | created |
------|----------|----------|------|---------|
 '''
